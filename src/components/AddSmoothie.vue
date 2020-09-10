bc9a93986bc64ff9a579cba1b1e195be7c3f9564
Adam Kalimi <adam.kalimi@yahoo.com>
2020-09-10 18:05:07 +0800

    Added slugify package. Also able to now push new smoothies to the DB

--- src/components/AddSmoothie.vue
+++ src/components/AddSmoothie.vue
@@ -6,9 +6,27 @@
                 <label for="title">Smoothie Title</label>
                 <input type="text" name="title" v-model="title">
             </div>
+
+
+            <!-- <ul class="ingredients">
+                <li v-for="(ingredient, index) in ingredients" :key="index">
+                    <span class="chip">{{ ingredient }}
+                        <i class="material-icons clear" @click="deleteIngredient(index)">clear</i>
+                    </span>
+                    
+                </li>
+            </ul> -->
+
+            <div v-for="(ingredient, index) in ingredients" :key="index">
+                <label for="ingredient">Ingredient:</label>
+                <input type="text" name="ingredient" v-model="ingredients[index]">
+            </div>
+
+
             <div class="field add-ingredient">
                 <label for="add-ingredient">Add an ingredient</label>
-                <input type="text" name="add-ingredient">
+                <input type="text" name="add-ingredient" @keydown.tab.prevent="addIngredient" v-model="another">
+                <p v-if="feedback" class="red-text">{{ feedback }}</p>
             </div>
             <div class="field center-align">
                 <button class="btn pink"> Add Smoothie</button>
@@ -18,17 +36,63 @@
 </template>
 
 <script>
+
+import db from '@/firebase/init'
+import slugify from 'slugify'
+
 export default {
     name: 'AddSmoothie',
     data() {
         return {
-            title: null
+            title: null,
+            another: null,
+            ingredients: [],
+            feedback: null,
+            slug: null
         }
     },
     methods: {
         AddSmoothie() {
-            console.log(this.title)
-        }
+            // console.log(this.title)
+            if(this.title) {
+                this.feedback = null
+                this.slug = slugify(this.title, {
+                    replacement: '-',
+                    remove: /[$*_+~.()'"!\-:@]/g,
+                    lower: true
+                }),
+                // console.log(this.slug)
+                db.collection('smoothies').add({
+                    title: this.title,
+                    ingredients: this.ingredients,
+                    slug: this.slug
+                }).then(() => {
+                    this.$router.push({ name: 'Index'})
+                }).catch(err => {
+                    console.log(err)
+                })
+
+            } else {
+                this.feedback = 'You must enter a title'
+            }
+            
+        },
+        addIngredient() {
+            if (this.another) {
+                this.ingredients.push(this.another)
+                console.log(this.ingredients)
+                this.another = null
+                this.feedback = null
+            } else {
+                this.feedback = 'Please enter an ingredient to be added'
+            }
+        },
+        // deleteIngredient(index) {
+        //     console.log("Clear Ingredient", index)
+        //     this.ingredients = this.ingredients.filter( ingredient => {
+        //         return ingredient[index] !== index
+        //     })
+        // }
     }
 }
 </script>
@@ -47,6 +111,14 @@ export default {
 
     .add-smoothie .field {
         margin: 20px auto;
+    }
+
+    .ingredients li {
+        display: inline-block;
+    }
+
+    .ingredients .clear {
+        font-size: 1.3em;
     }
 
 </style>
